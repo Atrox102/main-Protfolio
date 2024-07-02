a66726584f700710a60a2abaa77172e6cb060f8e
Atrox <saif.ahmed1215@gmail.com>
2024-07-02 06:49:17 +0300

    adjusted some ratios on the parralax #3

--- components/main/Parallax.tsx
+++ components/main/Parallax.tsx
@@ -26,7 +26,7 @@ const Parallax: React.FC<ParallaxProps> = ({ type }) => {
     >
       <motion.h1
         style={{ y: yText }}
-        className="text-center text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 font-semibold lg:text-[72px] max-lg:text-[40px] max-lg:absolute max-lg:top-[40%]"
+        className="text-center text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 font-semibold lg:text-[72px] max-lg:text-[40px] max-lg:absolute max-lg:top-[50%]"
       >
         {type === "services" ? "What We Did?" : "Our Portfolio"}
       </motion.h1>
@@ -39,7 +39,7 @@ const Parallax: React.FC<ParallaxProps> = ({ type }) => {
         style={{
           y: yBg,
         }}
-        className="absolute bottom-10 w-full h-full max-lg:w-[150%]  max-lg:h-[600px]"
+        className="absolute bottom-10 w-full h-full max-lg:w-[150%]  max-lg:h-[400px]"
       >
         <Image src={"/planets.png"} alt="" fill />
       </motion.div>
